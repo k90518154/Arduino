3c3cc495c10f2c020018b04b5d7f0683398bb864
k90518154 <37441887+k90518154@users.noreply.github.com>
2018-05-18 20:44:36 +0800

    Add files via upload

---
+++ Color.ino
@@ -0,0 +1,49 @@
+int redPin = 11;
+int greenPin = 10;
+int bluePin = 9;
+void setup()
+{
+  pinMode(redPin,OUTPUT);
+   pinMode(greenPin,OUTPUT);
+    pinMode(bluePin,OUTPUT);
+}
+void loop()
+{
+  analogWrite(redPin,255);
+  analogWrite(greenPin,0);
+  analogWrite(bluePin,0);
+  delay(500);
+   analogWrite(redPin,255);
+  analogWrite(greenPin,90);
+  analogWrite(bluePin,0);
+  delay(500);
+   analogWrite(redPin,255);
+  analogWrite(greenPin,255);
+  analogWrite(bluePin,0);
+  delay(500);
+   analogWrite(redPin,0);
+  analogWrite(greenPin,255);
+  analogWrite(bluePin,0);
+  delay(500);
+   analogWrite(redPin,0);
+  analogWrite(greenPin,0);
+  analogWrite(bluePin,255);
+  delay(500);
+   analogWrite(redPin,0);
+  analogWrite(greenPin,255);
+  analogWrite(bluePin,255);
+  delay(500);
+   analogWrite(redPin,255);
+  analogWrite(greenPin,0);
+  analogWrite(bluePin,255);
+  delay(500);
+   analogWrite(redPin,0);
+  analogWrite(greenPin,0);
+  analogWrite(bluePin,0);
+  delay(500);
+   analogWrite(redPin,255);
+  analogWrite(greenPin,255);
+  analogWrite(bluePin,255);
+  delay(500);
+  }
+
